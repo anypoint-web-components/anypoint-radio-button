fb24a888622b453ee91f35d01a1b7ef41a81fc6c
Pawel <jarrodek@gmail.com>
2019-08-14 01:14:52 -0700

    docs: updating documentation and demo page

--- README.md
+++ README.md
@@ -4,9 +4,16 @@
 
 # `<anypoint-radio-button>`
 
-Accessible radio button and radio buttons group for Anypoint platform
+Accessible radio button and radio button group for Anypoint platform.
+
+Radio buttons are used to select one of predefined options.
+Radio buttons should be used when the user must see all available options. Consider using a dropdown list if the options can be collapsed.
+
+See [Radio buttons](https://material.io/design/components/selection-controls.html#radio-buttons) documentation in Material Design documentation for principles and anatomy of radio buttons.
 
 ## Usage
+
+### Installation
 
 ```
 npm i --save @anypoint-web-components/anypoint-radio-button
---
+++ demo/index.js
@@ -0,0 +1,77 @@
+import { html } from 'lit-html';
+import { ArcDemoPage } from '@advanced-rest-client/arc-demo-helper/ArcDemoPage.js';
+import '@advanced-rest-client/arc-demo-helper/arc-interactive-demo.js';
+import '@anypoint-web-components/anypoint-checkbox/anypoint-checkbox.js';
+import '../anypoint-radio-button.js';
+import '../anypoint-radio-group.js';
+
+import introTemplate from './templateIntroduction.js';
+import usageTemplate from './templateUsage.js';
+
+class ComponentDemo extends ArcDemoPage {
+  constructor() {
+    super();
+    this.initObservableProperties([
+      'demoDisabled'
+    ]);
+    this.componentName = 'anypoint-radio-button';
+    this.demoStates = ['Normal'];
+    this._toggleMainOption = this._toggleMainOption.bind(this);
+  }
+
+  _toggleMainOption(e) {
+    const { name, checked } = e.target;
+    this[name] = checked;
+  }
+
+  _demoTemplate() {
+    const {
+      demoStates,
+      darkThemeActive,
+      demoDisabled
+    } = this;
+    return html`<section class="documentation-section">
+      <h3>Interactive demo</h3>
+      <p>
+        This demo lets you preview the radio button in a radio group with various
+        configuration options.
+      </p>
+      <arc-interactive-demo
+        .states="${demoStates}"
+        ?dark="${darkThemeActive}"
+      >
+        <label id="mainLabel">Radio group</label>
+        <anypoint-radio-group
+          aria-labelledby="mainLabel"
+          slot="content"
+          ?disabled="${demoDisabled}"
+          >
+          <anypoint-radio-button name="fruit">Apple</anypoint-radio-button>
+          <anypoint-radio-button name="fruit">Banana</anypoint-radio-button>
+          <anypoint-radio-button name="fruit">Orange</anypoint-radio-button>
+        </anypoint-radio-group>
+
+        <label slot="options" id="mainOptionsLabel">Options</label>
+        <anypoint-checkbox
+          aria-describedby="mainOptionsLabel"
+          slot="options"
+          name="demoDisabled"
+          @change="${this._toggleMainOption}"
+          >Disabled</anypoint-checkbox
+        >
+      </arc-interactive-demo>
+    </section>`;
+  }
+
+  contentTemplate() {
+    return html`
+      <h2>Anypoint Radio Button</h2>
+      ${this._demoTemplate()}
+      ${introTemplate}
+      ${usageTemplate}
+    `;
+  }
+}
+const instance = new ComponentDemo();
+instance.render();
+window.demo = instance;
